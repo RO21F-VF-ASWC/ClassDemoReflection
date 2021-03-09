8cf2c2bebf70e8e93c3f3ad59c63cad240d334a2
Peter Levinsky <pele@easj.dk>
2021-03-09 09:06:12 +0100

    Add project files.

---
+++ ClassDemoReflection/ReflectionWorker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace ClassDemoReflection
+{
+    internal class ReflectionWorker
+    {
+        public void Start()
+        {
+            Object o = new Person() { Id = 9, Name = "Peter" };
+            Console.WriteLine("Person object ==> " + o);
+
+            /*
+            * Reflection
+            */
+            Type t = o.GetType();
+
+
+
+
+        }
+
+
+    }
+
+    internal class Person
+    {
+        public String Name { get; set; }
+
+        public int Id { get; set; }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Id)}: {Id}";
+        }
+    }
+
+}
